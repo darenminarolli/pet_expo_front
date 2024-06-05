e1e61ad3fc5fbf0da4f657582edcec959bbf63fc
darenminarolli <Daren>
2024-06-05 22:15:05 +0200

    not found page

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import './App.css'
 import HomePage from './pages/HomePage'
 import PetsPage from './pages/PetsPage'
 import AdminPage from './pages/AdminPage'
+import NotFound from './pages/NotFound'
 function App() {
   const router = createBrowserRouter(createRoutesFromElements(
     <>
     <Route path='/' element={<HomePage/>} />
     <Route path='/pets/:type' element={<PetsPage/>} />
     <Route path='/admin' element={<AdminPage/>} />
+    <Route path='*' element={<NotFound/>} />
     </>
   ))
   return (
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../layouts/Layout'
+
+const NotFound = () => {
+  return (
+    <Layout>
+      <h1 className='secondary-header'>Sorry the page you are looking for does not exist</h1>
+    </Layout>
+  )
+}
+
+export default NotFound
